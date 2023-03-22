24bd541daf7221745dc4118b6beffa47ac7c4e86
Daniel Pichinil <daniel.pichinil@gmail.com>
2023-03-22 14:17:25 -0300

    add initial files project

---
+++ src/test/java/cl/dpichinil/test/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.dpichinil.test.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
